b097b0027e32e6b75f3b9b9ff4f8705f7f584ac9
rogerprz <rogerprz@github.com>
2020-12-08 23:03:53 -0800

    add dark green to geometry

--- CrazyRoad/GameViewController.swift
+++ CrazyRoad/GameViewController.swift
@@ -10,10 +10,46 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    
+    var scene: SCNScene!
+    var sceneView: SCNView!
+    
+    var cameraNode = SCNNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupScene()
+        setupFloor()
+        setupCamera()
+    }
+    
+    func setupScene() {
+        sceneView = (view as! SCNView)
+        scene = SCNScene()
         
+        sceneView.scene = scene
+    }
+    
+    func setupFloor() {
+        let floor = SCNFloor()
+        floor.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/darkgrass.png")
+        floor.firstMaterial?.diffuse.wrapS = .repeat // repeat our texture and not stetch over geometry
+        floor.firstMaterial?.diffuse.wrapT = .repeat
+        floor.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(12.5, 12.5, 12.5) // 4 dimensional matrix
 
+        
+        floor.reflectivity = 0.0
+        
+        let floorNode = SCNNode(geometry: floor)
+        scene.rootNode.addChildNode(floorNode)
+    }
+    
+    func setupCamera() {
+        cameraNode.camera = SCNCamera()
+//        x 0 keep camera on middle of x axis y:10 above the ground z: camera center
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 0)
+//        make camera face downward y:0 face the floor, z:
+        cameraNode.eulerAngles = SCNVector3(x: -.pi/2, y: 0, z: 0)
+        scene.rootNode.addChildNode(cameraNode)
     }
 }
